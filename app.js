944eb0dc82b11c9d8e8e652dee3827cdbbee18ba
cris-a <arcilacristian@gmail.com>
2024-06-11 18:19:52 -0400

    uridb

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const BaileysProvider = require('@bot-whatsapp/provider/baileys');
 const MongoAdapter = require('@bot-whatsapp/database/mongo');
 const ChatGPTClass = require('./chatgpt.class');
 
-const MONGO_DB_URI = process.env.MONGODB_URI;
 const MONGO_DB_NAME = 'db_bot';
 const createBotGPT = async ({ provider, database }) => {
   return new ChatGPTClass(database, provider);
@@ -14,7 +13,7 @@ const createBotGPT = async ({ provider, database }) => {
 
 const main = async () => {
   const adapterDB = new MongoAdapter({
-    dbUri: MONGO_DB_URI,
+    dbUri: process.env.MONGODB_URI,
     dbName: MONGO_DB_NAME,
   });
   const adapterProvider = createProvider(BaileysProvider);
